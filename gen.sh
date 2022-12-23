a7533c553aba2948f2a23b6c3a3b194a0d65c9b1
David Garner <dprgarner@gmail.com>
2022-12-23 20:17:17 +0000

    Update code-generator for cli

--- gen.sh
+++ gen.sh
@@ -9,10 +9,7 @@ touch $DIR/small.txt
 touch $DIR/large.txt
 cat > "$DIR"/src/main.rs << EOF
 use std::error::Error;
-use std::io;
-
-const SOLVE: char = 'A';
-// const SOLVE: char = 'B';
+use std::{env, io};
 
 fn solve_a(input: impl Iterator<Item = String>) -> Result<i32, Box<dyn Error>> {
     for line in input {
@@ -28,12 +25,19 @@ fn solve_b(input: impl Iterator<Item = String>) -> Result<i32, Box<dyn Error>> {
     Ok(2)
 }
 
+fn get_problem() -> Option<char> {
+    let arg = env::args().nth(1)?;
+    let problem = arg.chars().next();
+    problem
+}
+
 fn main() {
+    let problem = get_problem().expect("Usage:\n  cargo run -- a");
     let input = io::stdin().lines().map(|line| line.expect("IO error"));
 
-    match SOLVE {
-        'A' => println!("a soln: {}", solve_a(input).unwrap()),
-        'B' => println!("b soln: {}", solve_b(input).unwrap()),
+    match problem {
+        'a' => println!("a soln: {}", solve_a(input).unwrap()),
+        'b' => println!("b soln: {}", solve_b(input).unwrap()),
         _ => panic!("Unrecognised soln"),
     }
 }
@@ -42,4 +46,5 @@ EOF
 
 echo "Created $DIR. To run:"
 echo "  cd $DIR"
-echo "  cargo run"
+echo "  cat small.txt | cargo run -- a"
+echo "  cat large.txt | cargo run -- b"
